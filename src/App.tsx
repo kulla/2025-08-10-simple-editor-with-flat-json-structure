f5d130d79155aaa1cfc94918d51cc7eac08e9461
Stephan Kulla <git.mail@kulla.me>
2025-08-10 21:35:51 +0200

    feat: Add basic rendering

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const defaultContent: Content = {
   type: 'content',
   children: [
     {
-      type: 'content',
+      type: 'text',
       children: [
         { type: 'paragraph', text: 'Welcome! This is a simple editor' },
         {
@@ -16,7 +16,7 @@ const defaultContent: Content = {
     {
       type: 'mutiple-choice-exercise',
       question: {
-        type: 'content',
+        type: 'text',
         children: [
           { type: 'paragraph', text: 'What is React primarily used for?' },
         ],
@@ -39,14 +39,73 @@ const defaultContent: Content = {
   ],
 }
 
+function render(element: Element, key?: React.Key) {
+  switch (element.type) {
+    case 'content':
+      return renderContent(element as Content, key)
+    case 'mutiple-choice-exercise':
+      return renderMultipleChoiceQuestion(element as MutipleChoiceExercise, key)
+    case 'text':
+      return renderText(element as Text, key)
+    case 'paragraph':
+      return renderParagraph(element as Paragraph, key)
+    default:
+      return null
+  }
+}
+
+function renderContent(content: Content, key?: React.Key) {
+  return (
+    <section key={key} className="rounded-xl border-2 p-4">
+      {content.children.map((child, idx) => render(child, idx))}
+    </section>
+  )
+}
+
+function renderText(text: Text, key?: React.Key) {
+  return (
+    <div key={key}>
+      {text.children.map((p, idx) => renderParagraph(p, idx))}
+    </div>
+  )
+}
+
+function renderParagraph(paragraph: Paragraph, key?: React.Key) {
+  return <p key={key}>{paragraph.text}</p>
+}
+
+function renderMultipleChoiceQuestion(
+  exercise: MutipleChoiceExercise,
+  key?: React.Key,
+) {
+  return (
+    <div key={key} className="card bg-info-content">
+      <div className="card-body">
+        <h2 className="card-title">Multiple Choice Question</h2>
+        <div>{renderText(exercise.question)}</div>
+        <ul className="list-none pl-0">
+          {exercise.answers.map((answer, idx) => (
+            <li key={idx.toString()} className="flex items-center gap-4">
+              <input type="checkbox" className="checkbox" />
+              {render(answer.text, idx)}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
 export default function App() {
   return (
     <main className="prose p-10">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Editor:</h1>
+      {renderContent(defaultContent)}
     </main>
   )
 }
+
+type Element = Content | MutipleChoiceExercise | Text | Paragraph
 
 interface Content {
   type: 'content'
@@ -65,7 +124,7 @@ interface MultipleChoiceAnswer {
 }
 
 interface Text {
-  type: 'content'
+  type: 'text'
   children: Paragraph[]
 }
 
